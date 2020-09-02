57677feb0934c8ff7cb49df1f3511eb42d9296e8
Harsh Jha <harshjhaa11@gmail.com>
2020-09-03 02:51:45 +0530

    Final Working App

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -28,7 +28,7 @@ const Banner = () => {
             style={{
                 backgroundSize: 'cover',
                 backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
-                backgroundPosition: 'center center'
+                // backgroundPosition: 'center center'
             }}>
             <div className="banner-fade-top"></div>
             <div className="banner-contents">
